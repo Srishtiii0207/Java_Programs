8b4a223557624459ec35d081820db3e69fa5346c
Srishtiii0207 <142256953+Srishtiii0207@users.noreply.github.com>
2023-08-21 09:47:23 +0530

    Create Program_to_calculate_Sum_of_two_numbers.java

---
+++ Program_to_calculate_Sum_of_two_numbers.java
@@ -0,0 +1,12 @@
+package Majorsjava;
+
+
+public class Sum {
+    public static void main(String[] args) {
+
+        // add three numbers
+        int num1=3 , num2= 5, num3 = 19;
+        int sum = num1 + num2 + num3;
+        System.out.println("The sum of three numbers is: "+ sum);
+    }
+}
